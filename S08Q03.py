4395102ac110b77e3f9c30c699dd18d4aeb4a7ef
RaviPrakashMP <54932206+RaviPrakashMP@users.noreply.github.com>
2019-10-03 03:48:05 +0530

    Add files via upload

---
+++ S08Q03.py
@@ -0,0 +1,16 @@
+
+number = int(input("Enter a number:"))
+ 
+if number == 5:
+    number = "0000111122223333444"
+    print(number)
+
+if number == 3:
+    number = "001122"
+    print(number)
+
+else:
+    print("None")
+    
+
+             
