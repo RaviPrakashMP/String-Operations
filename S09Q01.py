a2243268ba9ba8cef2ee5425b3abbc026d8be08b
RaviPrakashMP <54932206+RaviPrakashMP@users.noreply.github.com>
2019-10-04 21:35:07 +0530

    Add files via upload

---
+++ S09Q01.py
@@ -0,0 +1,46 @@
+header = input("Enter the webpage title:")
+line1 = "<html>"
+line2 = "  <head>"
+line3 = "      <title>"
+
+subheader = input("Enter the subheader:")
+
+line4 = "    </title>"
+line5 = " </head>"
+
+line6 = "<body>"
+line7 = "    <h1> Webpage Banner </h1>"
+
+line8 = "    <p> This is sample story"
+line9 = "     for my first webpage"
+
+line10 = "    </p>"
+
+line11 = "    <p> This is ahow my sample"
+line12= "     story ends"
+
+line13 = "    </p>"
+
+line14 = " </body>"
+
+line15 = "</html>"
+
+print("                       ",header)
+print(line1)
+print(line2)
+print(line3)
+print("                       ",subheader)
+print(line4)
+print(line5)
+print(line6)
+print(line7)
+print(line8)
+print("  ",line9)
+print(line10)
+print(line11)
+print("  ",line12)
+print(line13)
+print(line14)
+print(line15)
+
+
